1d0b3abe9dd33efac7ff2a911762ea5b5ba2c4c5
Austin Kearns <austinkearns47@gmail.com>
2023-04-18 18:43:04 -0700

    personal config

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,7 +1,7 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
+    -- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
@@ -9,4 +9,13 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim"},
+    config = function()
+      require("todo-comments").setup{}
+    end,
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+  }
 }
---
+++ plugins/zen-mode.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/zen-mode.nvim",
+  lazy = false,
+  keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen mode"} }
+  -- config = function()
+  --   require("zen-mode").setup()
+  -- end,
+}
